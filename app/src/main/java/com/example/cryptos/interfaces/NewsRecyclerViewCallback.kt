b5fd1b6e6744e1a0cbdb7ea659f92bd13ef4ef70
fredy puente <frealpues@gmail.com>
2020-11-14 15:26:40 -0300

    delete class

--- app/src/main/java/com/example/cryptos/interfaces/NewsRecyclerViewCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cryptos.interfaces
-
-import com.example.cryptos.network.model.Article
-
-interface NewsRecyclerViewCallback {
-    fun onRecycleViewItemClick(article: Article, position: Int)
-}
